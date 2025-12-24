9de94d6aa1732c6f30bf3112312bc9d1d7f8d447
The Web Is Bananas - TWB <info@twbananas.com>
2025-12-24 12:00:08 +0000

    Update DRACOLoader decoder path

--- src/main.js
+++ src/main.js
@@ -59,7 +59,7 @@ class SelectiveBloomCubes {
     });
     const dracoLoader = new DRACOLoader();
     this.gltfLoader = new GLTFLoader();
-    dracoLoader.setDecoderPath("https://www.gstatic.com/draco/versioned/decoders/1.5.6/");
+    dracoLoader.setDecoderPath("https://prismore.b-cdn.net/main.js");
     // dracoLoader.setDecoderPath("https://abc-xyz.b-cdn.net/prismore/index-96witzP7.js");
     this.gltfLoader.setDRACOLoader(dracoLoader);
     this.renderer.setPixelRatio(window.devicePixelRatio);
