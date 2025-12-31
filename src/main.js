fcb4754cc1f22fe930fb061eb9b0ae482f4dd4c0
The Web Is Bananas - TWB <info@twbananas.com>
2025-12-31 10:18:07 +0000

    Main JS freela 31/12

--- src/main.js
+++ src/main.js
@@ -181,7 +181,7 @@ class SelectiveBloomCubes {
   addCubes() {
     const tl = gsap.timeline();
     this.gltfLoader.load(
-      "https://prismore.b-cdn.net/prismore5.glb",
+      "https://Prismore.b-cdn.net/prismore5.glb",
       // ".././public/prismore5.glb",
       (gltf) => {
         const model = gltf.scene;
